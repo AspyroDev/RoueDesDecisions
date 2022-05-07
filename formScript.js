f2210166933f4c0cf53daf38398f5d0b494317d9
AspyroDev <aspyrodev@gmail.com>
2022-05-07 22:47:39 +0200

    Fonctionnalité d'ajout d'éléments à la liste des choix (+emoji correspondant)

---
+++ formScript.js
@@ -0,0 +1,22 @@
+function addToChoiceList() {
+    // Get Emoji from Form
+    var emojis = document.getElementsByTagName("select");
+    var picked = emojis[0].selectedOptions[0].value;
+    
+    // Get Text from Form
+    var inputBox = document.getElementsByTagName("input");
+    var choice = inputBox[0].value;
+    
+    // Add Choice to Page Content
+    var tableRow = document.createElement("tr");
+
+    var tableEmojiData = document.createElement("td");
+    tableEmojiData.innerHTML = picked;
+    
+    var tableTextData = document.createElement("td");
+    tableTextData.innerHTML = choice;
+    
+    tableRow.appendChild(tableEmojiData);
+    tableRow.appendChild(tableTextData);
+    document.getElementsByTagName("table")[0].appendChild(tableRow);
+}
